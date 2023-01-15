fa53295e20f25263e82d7e9f4a5b253bb318ce41
yonago_katsushi <yonago.katsushi@gmail.com>
2023-01-16 01:13:30 +0900

    3章まで

--- nuxt-portfolio/nuxt.config.ts
+++ nuxt-portfolio/nuxt.config.ts
@@ -5,4 +5,13 @@ export default defineNuxtConfig({
     modules: [
     "@nuxt/content",
     ],
+    css: [
+
+        "~/assets/styles/common.css",
+        "~/assets/styles/layout.css",
+        "~/assets/styles/index.css",
+        "~/assets/styles/blog.css",
+        "~/assets/styles/contact.css",
+        "~/assets/styles/singleblog.css",
+    ]
 })
