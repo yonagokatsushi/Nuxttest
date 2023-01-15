3005663a2d39aca56efd9d2a266f8467a036803d
yonago_katsushi <yonago.katsushi@gmail.com>
2023-01-16 01:31:42 +0900

    表示調整

--- nuxt-portfolio/pages/blog/index.vue
+++ nuxt-portfolio/pages/blog/index.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="">
+  <div class="wrapper">
 
 
-    <div class="">
+    <div class="container">
       <h1>テストブログ</h1>
       <p>てすてすてすと</p>
       <ul>
-        <li v-for="singleData in data" :key="singleData.id">
+        <li v-for="singleData in data" :key="singleData.id" class="blogCard">
           <article>
             <h2>{{ singleData.title }}</h2>
             <p>{{ singleData.excerpt }}</p>
             <date>投稿日時：({{ singleData.date }})</date>
             <NuxtLink :to="singleData._path">続きを読む</NuxtLink>
           </article>
-          <div class="image">
+          <div class="blogImg">
             <img :src="singleData.image" :alt="singleData.excerpt">
           </div>
         </li>
@@ -24,9 +24,23 @@
 </template>
 
 <script setup>
+const blogsPerPage = 5
 
+const { data } = await useAsyncData("blogQuery", () => 
+    queryContent("/blog")
+    .sort({ id: -1 })
+    .limit(blogsPerPage)
+    .find()
+)
 
-  const { data } = await useAsyncData("blogQuery", () =>
-    queryContent("/blog").sort({ id: -1 }).find()
-  )
+const allBlogs = await queryContent("/blog").find()
+
+const numberPages = Math.ceil(allBlogs.length / blogsPerPage)
+
+useHead({
+    title: "ブログ",
+    meta: [
+        { name: "description", content: "ブログページです" }
+    ],
+})
 </script>
